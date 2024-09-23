b9f1f9f3479af15db8e86baaf91c43ea08bea4fb
henrypp <sforce5@mail.ru>
2024-09-23 21:48:09 +0500

    upd version

--- src/app.h
+++ src/app.h
@@ -5,7 +5,7 @@
 
 #define APP_NAME L"DbgVision"
 #define APP_NAME_SHORT L"dbgvision"
-#define APP_VERSION L"1.0"
-#define APP_VERSION_RES 1,0,0,0
+#define APP_VERSION L"1.1"
+#define APP_VERSION_RES 1,1,0,0
 #define APP_AUTHOR L"Henry++"
 #define APP_COPYRIGHT L"(c) 2024 " APP_AUTHOR L". All Rights Reversed."
